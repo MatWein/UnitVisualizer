dae09483d26f14f09e4e7aad734302973971b166
Matthias Weinert <matthiasweinert@web.de>
2018-08-12 22:44:07 +0200

    Fixed NPE.

--- src/mw/unitv/utils/PackageDetector.java
+++ src/mw/unitv/utils/PackageDetector.java
@@ -1,9 +1,6 @@
 package mw.unitv.utils;
 
-import com.intellij.psi.JavaDirectoryService;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiDirectory;
-import com.intellij.psi.PsiPackage;
+import com.intellij.psi.*;
 
 import java.util.Optional;
 
@@ -13,8 +10,17 @@ public class PackageDetector {
 		if (directoryService == null) {
 			return Optional.empty();
 		}
-		
-		PsiDirectory containingDirectory = psiClass.getContainingFile().getContainingDirectory();
+
+		PsiFile containingFile = psiClass.getContainingFile();
+		if (containingFile == null) {
+			return Optional.empty();
+		}
+
+		PsiDirectory containingDirectory = containingFile.getContainingDirectory();
+		if (containingDirectory == null) {
+			return Optional.empty();
+		}
+
 		PsiPackage sourcePackage = directoryService.getPackage(containingDirectory);
 		if (sourcePackage == null) {
 			return Optional.empty();
